cb996f5795115056284ecdf11ee82854d7dfe576
Lara Converso <laraconverso@gmail.com>
2022-06-09 23:49:08 -0300

    update

---
+++ Clases/c23s/src/main/java/com/example/c23s/C23sApplication.java
@@ -0,0 +1,13 @@
+package com.example.c23s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C23sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C23sApplication.class, args);
+	}
+
+}
---
+++ Clases/c23s/src/test/java/com/example/c23s/C23sApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.c23s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C23sApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
